7fb320b70841ea037f982b8ac4e1acd4f97b7248
Pasha1002 <kanavetspavel2001@gmail.com>
2023-05-20 22:09:18 +0300

    tusk dune

---
+++ main.cpp
@@ -0,0 +1,22 @@
+﻿
+
+
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int a;
+	cout << "Введіть число (від 2 до 9): ";
+	cin >> a;
+
+	for (int i = 2; i <= 9; ++i) {
+		int sum = a * i;
+		cout << a << "x" << i << "=" << sum << endl;
+	}
+
+	return 0;
+}

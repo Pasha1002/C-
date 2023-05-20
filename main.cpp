124a2e261a6ef927db889c142f4f9c5ed7664628
Pasha1002 <kanavetspavel2001@gmail.com>
2023-05-20 21:24:18 +0300

    tusk dune

---
+++ main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int x;
+	cout << "Введіть число: ";
+	cin >> x;
+	int y;
+	cout << "Введіть степінь числа: ";
+	cin >> y;
+	int sum;
+	sum = pow(x, y);
+	cout << "Число: " << x << " в степені: " << y << ": " << sum << endl;
+	return 0;
+}

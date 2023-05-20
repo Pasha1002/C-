cc24ce84e05a8c7d2a89cf3bc5d9acaba265bf6f
Pasha1002 <kanavetspavel2001@gmail.com>
2023-05-20 22:19:23 +0300

    tusk dune

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <windows.h>
+using std::endl;
+using std::cout;
+using std::cin;
+using std::string;
+using namespace std;
+
+
+
+int main() {
+	SetConsoleOutputCP(1251);
+	cout << "Не зрозумыв завдання";
+	
+	return 0;
+}

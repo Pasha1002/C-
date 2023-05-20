ce49c71745d76d1bdda06057796aaa9c0d6d81c4
Pasha1002 <kanavetspavel2001@gmail.com>
2023-05-20 21:04:09 +0300

    tusk dune

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <windows.h>
+using std::endl;
+using std::cout;
+using std::cin;
+using std::string;
+using namespace std;
+
+
+
+int main() {
+	SetConsoleOutputCP(1251);
+	
+		cout << "Не розібрався в задачі ";
+	return  0;
+}
